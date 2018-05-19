7f55bdb74acb8798610d63372a09c2364b87233b
John Lima <johnmaykbrito@gmail.com>
2018-05-19 01:11:18 -0300

    Problem solved
    I'm just dumb as fuck haha

--- target/generated-sources/annotations/com/mycompany/personaltech/entities/PersonalTrainer_.java
+++ target/generated-sources/annotations/com/mycompany/personaltech/entities/PersonalTrainer_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-05-18T23:29:49")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-05-19T01:05:01")
 @StaticMetamodel(PersonalTrainer.class)
 public class PersonalTrainer_ { 
 
